216d8c1cd02533ac8ab864f0f0187b133008857c
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-09-27 11:59:42 +0200

    higher order components

--- src/container/App.js
+++ src/container/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react' //responsible for rendering anything to
 import classes from './App.css'
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
+import WithClass from '../hoc/WithClass'
 
 //The definition of the App component as a class.
 class App extends Component {
@@ -79,14 +80,14 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <Cockpit
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonHandler} />
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
