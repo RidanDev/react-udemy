9f2fe2e8ac7657ae724745da73a29c63f891f037
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-01 14:30:52 +0200

    reusing the Backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Aux from '../../hoc/Auxiliary'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    //adjacent jsx elements
-    //per risolvere l'errore o ritorniamo un array e diamo ad ogni elemento una chiave
-    //oppure si può creare un auxiliary higher order component (consigliato)
-    //come alternativa si potrebbe wrappare tutto con un <div> di cui non abbiamo però bisogno. 
-    <Aux>
-        <Toolbar /> 
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
