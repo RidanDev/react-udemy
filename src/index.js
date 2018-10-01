1a2f4262d6ad6376eb340a63a6765e5c90f5864e
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-01 17:45:03 +0200

    try commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'; //the .js extension is omitted
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//The App component defined in the App.js file
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
