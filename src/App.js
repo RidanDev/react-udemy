9186ebb2f4ac9f1539763b40a987885ad4fa3aa0
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-02 17:53:49 +0200

    removing old interceptors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 class App extends Component {
-
   render() {
     return (
       <div>
