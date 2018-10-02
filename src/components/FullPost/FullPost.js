e85b8495de2f641f7959a576a24f5b1eec1eacb1
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-02 11:44:18 +0200

    fetching data on update

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+
+    //il secondo if mi assicura di richiamare il metodo se e solo se
+    //loadedPost è null oppure se loadedPost ha già dei dati ma l'id che ricevo
+    //è diverso da quello che ho in loadedPost. Questo mi evita un loop 
+    //infinito di richieste (visibile tramite network su console)
+    componentDidUpdate() {
+        if (this.props.id) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        //console.log(response)
+                        this.setState({ loadedPost: response.data })
+                    })
+            }
+        }
+    }
+
     render() {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
         if (this.props.id) {
+            post = <p style={{ textAlign: 'center' }}>Loading...</p>;
+        }
+        if (this.state.loadedPost) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
