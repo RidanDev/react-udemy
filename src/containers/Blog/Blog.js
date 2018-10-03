a8c48dd08d97d740ad11dab383cccc0e0114585a
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-03 14:50:34 +0200

    using Switch to load a single Route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
 
 import './Blog.css';
 import Posts from './Posts/Posts'
@@ -29,12 +29,15 @@ class Blog extends Component {
                             }}>New Post</NavLink></li>
                         </ul>
                     </nav>
-                </header>
-                {/*<Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path="/" render={() => <h1>Home 2</h1>} />*/}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                </header>                
+                {/*Switch permette di eseguire il primo Route che fa match
+                e non tutti (infatti senza Switch /:id verrebbe eseguito in quanto
+                interpreterebbe new-post come un id)*/}
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" exact component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
