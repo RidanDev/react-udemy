e95e9f8df7f5b14d1f9f5a0dba676dd728e9c08d
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-03 12:43:44 +0200

    extracting route parameters

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -12,10 +12,11 @@ class FullPost extends Component {
     //loadedPost è null oppure se loadedPost ha già dei dati ma l'id che ricevo
     //è diverso da quello che ho in loadedPost. Questo mi evita un loop 
     //infinito di richieste (visibile tramite network su console)
-    componentDidUpdate() {
-        if (this.props.id) {
+    componentDidMount() {
+        console.log(this.props)
+        if (this.props.match.params.id) {
             if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         //console.log(response)
                         this.setState({ loadedPost: response.data })
