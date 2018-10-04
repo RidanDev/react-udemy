ce3225d0c9d994a83edf7fa4ec99300fb2d8beb9
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-04 14:55:21 +0200

    setting up Routing & Routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+
 import './index.css';
 import App from './App'; //the .js extension is omitted
 import registerServiceWorker from './registerServiceWorker';
 
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
+
 //The App component defined in the App.js file
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
