a6e20947ca680cafb656b000fd62dac2e51673a3
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-04 16:22:13 +0200

    passing ingredients with Query Params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,7 +11,18 @@ class Checkout extends Component {
             bacon: 1
         }
     }
-    
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search)
+        const ingredients = {}
+        for (let param of query.entries()) {
+            //['salad', '1']
+            //console.log("param: " + param)
+            ingredients[param[0]] = +param[1]
+        }
+        this.setState({ ingredients: ingredients })
+    }
+
     checkoutCancelledHandler = () => {
         this.props.history.goBack()
     }
@@ -23,7 +34,7 @@ class Checkout extends Component {
     render() {
         return (
             <div>
-                <CheckoutSummary 
+                <CheckoutSummary
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
