e998bd0ee2423246aa9b1dedbb29e327dde4bb18
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-06 12:12:44 +0200

    to fix

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -18,11 +18,25 @@ const input = (props) => {
                 {...props.elementConfig}
                 value={props.value} />;
             break;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                    
+                </select>
+            );
+            break;
         default:
             inputElement = <input
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value} />
+                value={props.value} />;
     }
 
     return (
