cb69c9de55b07887b76ae4f135fb81d59b46346b
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-11 11:28:57 +0200

    preparing the folder structure

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createStore } from 'redux'
 import './index.css';
 import App from './App'; //the .js extension is omitted
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer'
+import reducer from './store/reducers/burgerBuilder'
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
