f4126a311d8fd238c82f075c2808f8488198b7f0
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-15 12:06:52 +0200

    adding an Auth Form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,14 +5,10 @@ import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem
-            link="/"
-            exact>Burger Builder
-        </NavigationItem>
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
+        <NavigationItem link="/auth">Authenticate</NavigationItem>
 
-        <NavigationItem
-            link="/orders">Orders
-        </NavigationItem>
     </ul>
 )
 
